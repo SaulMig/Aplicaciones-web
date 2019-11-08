f10794549a96e12dda08d4496fe2aa70cbadfa1b
SaulMig <38225406+SaulMig@users.noreply.github.com>
2019-11-08 00:25:10 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 define('DS',DIRECTORY_SEPARATOR);
 define('ROOT',realpath(dirname(__FILE__)).DS);
-define('URL',"http://localhost/Aplicaciones-web/");
+define('URL',"http://localhost/sistema_inventario/");
 
 
 
